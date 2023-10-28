e4bfb53efdc21985255aa8d8b987b38c29b50c99
likes-front-end <137172921+it-tinyangel@users.noreply.github.com>
2023-10-28 15:21:36 +0300

    Convert to String Array (use `join()` and `split()` methods)

---
+++ script.js
@@ -0,0 +1,11 @@
+// The function to convert an array of numbers to an array of strings.
+function arrToString(arr) {
+	return resultString = arr.join(', ').split(', ');
+}
+// Call the function and write the result in a variable.
+const arr1 = arrToString([1, 2, 3]);
+const arr2 = arrToString([10, 200, 3333]);
+
+// Outputing the result to the console.
+console.log(`Converted arr1 array:`, arr1);
+console.log(`Converted arr2 array:`, arr2);
